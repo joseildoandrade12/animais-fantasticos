37a927f7e318cf8b8e577cf20159b102513cb6fb
joseildoandrade12 <joseildoandrade1405@gmail.com>
2025-01-03 20:28:15 -0300

    ScrollSuave tranformado em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,28 +1,32 @@
-export default function initScrollSuave() {
-    const linksInternos = document.querySelectorAll(
-        '[data-menu="suave"] a[href^="#"]'
-    );
-    
-    function scrollToSection(event) {
+export default class ScrollSuave {
+    constructor(links, options) {
+        this.linksInternos = document.querySelectorAll(links);
+        if (this.options === undefined) {
+            this.options = { behavior: "smooth", block: "start" };
+        } else {
+            this.options = options;
+        }
+
+        this.scrollToSection = this.scrollToSection.bind(this);
+    }
+
+    scrollToSection(event) {
         event.preventDefault();
         const href = this.getAttribute("href");
         const section = document.querySelector(href);
-
-        section.scrollIntoView({
-            behavior: "smooth",
-            block: "start",
-        });
-
-        //uma das formas
-        // const topo = section.offsetTop;
-
-        // window.scrollTo({
-        //     top: topo,
-        //     behavior: 'smooth',
-        // })
+        section.scrollIntoView(this.options);
     }
 
-    linksInternos.forEach((link) => {
-        link.addEventListener("click", scrollToSection);
-    });
+    addLinkEvent() {
+        this.linksInternos.forEach((link) => {
+            link.addEventListener("click", this.scrollToSection);
+        });
+    }
+
+    init() {
+        if (this.linksInternos.length) {
+            this.addLinkEvent();
+        }
+        return this;
+    }
 }
